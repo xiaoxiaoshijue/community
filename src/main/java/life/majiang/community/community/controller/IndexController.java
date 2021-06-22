d2efb1ca04e4f4d340c6622e331073b921613815
xurenhai1 <1318195220@qq.com>
2021-06-22 23:35:04 +0800

    bootstrap快速写出导航栏

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
+public class IndexController {
+    @GetMapping("/")
+    public String index(){return "index";}
+   /* public String hello(@RequestParam(name = "name") String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
-    }
+        return "index";
+    }*/
 }
