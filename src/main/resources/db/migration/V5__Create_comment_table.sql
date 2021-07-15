609e3fe2c9e91e6745bbd52ad404c882bfd43ee9
xurenhai1 <1318195220@qq.com>
2021-07-15 15:35:31 +0800

    初步实现回复插入功能使用了json传输  用到postman进行测试

---
+++ src/main/resources/db/migration/V5__Create_comment_table.sql
@@ -0,0 +1,12 @@
+create table Comment
+(
+	id BIGINT auto_increment,
+	parent_id BIGINT not null,
+	type int not null,
+	commentator int not null,
+	gmt_create BIGINT not null,
+	gmt_modified BIGINT not null,
+	like_count BIGINT default 0 not null,
+	constraint COMMENT_PK
+		primary key (id)
+);
