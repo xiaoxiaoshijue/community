37c1c1d544c4a0fa15f29c1f0911cb0cb05660e4
xurenhai1 <1318195220@qq.com>
2022-04-12 03:51:39 +0800

    配置文件加密

---
+++ src/main/java/life/majiang/community/community/provider/JasyptUtil.java
@@ -0,0 +1,56 @@
+package life.majiang.community.community.provider;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+
+/**
+ * @author shuzhuo
+ * @date 2019/1/9 9:56
+ */
+public class JasyptUtil {
+
+    /**
+     * Jasypt生成加密结果
+     * @param password 配置文件中设定的加密密
+     * @param value 加密值
+     * @return
+     */
+    public static String encyptPwd(String password,String value){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(cryptor(password));
+        String result = encryptor.encrypt(value);
+        return result;
+    }
+
+    /**
+     * 解密
+     * @param password 配置文件中设定的加密密码
+     * @param value 解密密文
+     * @return
+     */
+    public static String decyptPwd(String password,String value){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(cryptor(password));
+        String result = encryptor.decrypt(value);
+        return result;
+    }
+
+    public static SimpleStringPBEConfig cryptor(String password){
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        return config;
+    }
+
+    public static void main(String[] args){
+        //加密
+        System.out.println(encyptPwd("jasypt","Xrh19780!"));
+        //解密
+        System.out.println(decyptPwd("jasypt","j04iGO7Rvjv4EzPreeEjo7tQg2ML5rHfTftffnfeO/tfoCWeC7L6YcrGigzY3F/yUUJnJb16e0+b8Kv/sVQRpUtn/7JKqAGN6Lve9H3cplY="));
+    }
+}
